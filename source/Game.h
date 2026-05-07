1d49c51c9904dc3624a448a1f9a6721e31681ebd
UnaRamita <Zelmarrodriguez97@gmail.com>
2026-05-06 22:02:51 -0300

    Tp2 MAVI 2 terminado

---
+++ source/Game.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef NDEBUG
+#include <vld.h>
+#endif
+#include "raylib.h"
+#include <box2d.h>
+#include <vector>
+#include "DynamicObject.h"
+#include "PhysicsManager.h"
+#include "StaticObjects.h"
+#include "Config.h"
+#include "Input.h"
+#include "Preview.h"
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	void inits();
+
+	void run();
+
+private:
+
+	staticObjects* ground;
+	staticObjects* wallLeft;
+	staticObjects* wallRight;
+
+	Preview* preview;
+
+	void load();
+	void inputs();
+
+	physicsManager* physics;
+	std::vector<staticObjects*> environment;//ambiente, suelo y paredes, dsp capaz le cambio el nombre de esto
+	std::vector<DynamicObject*> projectiles; // Lista para almacenar los proyectiles lanzados
+};
